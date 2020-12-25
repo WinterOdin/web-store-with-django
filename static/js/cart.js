0807e5ea30923752f6ca5147a58a74b3e4dada3f
WinterOdin <cczuryszkiewicz@gmail.com>
2020-12-25 22:09:33 +0100

    working on cart and checkout

---
+++ static/js/cart.js
@@ -0,0 +1,35 @@
+var updateBtn =document.getElementsByClassName("update-cart")
+
+for (i = 0; i < updateBtn.length; i++){
+    updateBtn[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action    = this.dataset.action
+        console.log(productId,action)
+        console.log(user)
+        if (user === "AnonymousUser"){
+            console.log("user not logged in")
+        }else{
+           updateUserOrder(productId, action)
+        }
+    })
+}
+function updateUserOrder(productId, action){
+    console.log("user created an order")
+
+    var url = 'updateItem'
+
+    fetch(url, {
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({'productId':productId, 'action':action})
+    })
+    .then((response) =>{
+        return response.json()
+    })
+    .then((data) =>{
+        console.log('data', data)
+    })
+}
