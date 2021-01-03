20e9e85b5fe82b8d13008f477ae5123deb3c6fbe
WinterOdin <cczuryszkiewicz@gmail.com>
2021-01-03 09:19:56 +0100

    working on tracing user behaviour
    -working cart for auth and unauth user
    -working checkout page
    -working latest products
    -improved loading times

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,23 +1,44 @@
 var updateBtn =document.getElementsByClassName("update-cart")
 
+
+
+
+
 for (i = 0; i < updateBtn.length; i++){
     updateBtn[i].addEventListener('click', function(){
         var productId = this.dataset.product
         var action    = this.dataset.action
-        console.log(productId,action)
-        console.log(user)
         if (user === "AnonymousUser"){
-            console.log("user not logged in")
+            addCookieItem(productId,action)
         }else{
            updateUserOrder(productId, action)
         }
     })
 }
-function updateUserOrder(productId, action){
-    console.log("user created an order")
 
+
+function addCookieItem(productId,action){
+    if(action == "add"){
+        if(cart[productId] === undefined){
+            cart[productId] = {'quantity':1}
+        }else{
+            cart[productId]['quantity'] += 1
+        }
+    }
+    if (action == "remove" || action =="delete"){
+        cart[productId]['quantity'] -= 1
+        if(cart[productId]['quantity'] <= 0){
+            delete cart[productId]
+        }
+    }
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
+}
+
+
+
+function updateUserOrder(productId, action){
     var url = 'http://127.0.0.1:8000/updateItem'
-    console.log(url)
     fetch(url, {
         method:'POST',
         headers:{
@@ -25,14 +46,11 @@ function updateUserOrder(productId, action){
             'X-CSRFToken':csrftoken,
         },
         body:JSON.stringify({'productId':productId, 'action':action})
-        
     })
     .then((response) =>{
         return response.json()
-        
     })
     .then((data) =>{
-        console.log('data', data)
         location.reload()
     })
 }
